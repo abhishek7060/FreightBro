d76e7918e11245d1c53abcf0f6e4f9aa5cfba76e
abhishek7060 <abhishek7060@gmail.com>
2021-03-11 01:08:37 +0530

    FreightBro-TestApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ApplicationTest';
+export class AppComponent implements OnInit {
+  public countries = [{ label1: "India", value1: "IN" }, { label1: "Japan", value1: "JA" }, { label1: "Germany", value1: "GR" }, { label1: "Russia", value1: "RU" }];
+  public optionValueLabel = "label1";
+  public optionValueName = "value1";
+  public selectedCountries = [];
+  ngOnInit() { }
+
+  onChange(event: any) {
+    console.log(event);
+    this.selectedCountries = event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MultiselectModule } from '@abhishek7060/multiselect';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MultiselectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
